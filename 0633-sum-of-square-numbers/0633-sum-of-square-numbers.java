5bdcfd6b1a7755fd807297979366235a952f688a
Vinay Jagannavar <vjagannavar@gmail.com>
2024-06-17 18:13:37 -0400

    Time: 3 ms (97.78%), Space: 40 MB (90.11%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
@@ -1,9 +1,8 @@
 class Solution {
     public boolean judgeSquareSum(int c) {
-        long max = (int) Math.sqrt(c);
 
         long left=0;
-        long right=max;
+        long right=(long) Math.sqrt(c);;
 
         while(left<=right){
             long sum = left * left + right * right;
