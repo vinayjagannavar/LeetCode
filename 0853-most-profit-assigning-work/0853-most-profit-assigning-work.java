43d8f312b4427628b7fb704cac767b6b532cf22b
Vinay Jagannavar <vjagannavar@gmail.com>
2024-06-17 22:14:31 -0400

    Time: 20 ms (62.04%), Space: 45.7 MB (39.31%) - LeetHub

--- 0853-most-profit-assigning-work/0853-most-profit-assigning-work.java
+++ 0853-most-profit-assigning-work/0853-most-profit-assigning-work.java
@@ -1,5 +1,5 @@
 class Solution {
-    class Pair{
+    class Pair implements Comparable<Pair>{
         int diff;
         int profit;
 
@@ -7,53 +7,34 @@ class Solution {
             this.diff = d;
             this.profit = p;
         }
+
+        public int compareTo(Pair p2){
+            return this.diff-p2.diff;
+        }
     }
 
     public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
-        int n= difficulty.length;
-        Pair[] job = new Pair[n];
-
-        for(int i=0; i<profit.length; i++){
-            job[i] = new Pair(difficulty[i],profit[i]);
+        int n = difficulty.length;
+        Pair[] jobs = new Pair[n];
+        for (int i = 0; i < n; i++) {
+            jobs[i] = new Pair(difficulty[i], profit[i]);
         }
-
-        Arrays.sort(job , (a,b) -> a.diff - b.diff);
+        Arrays.sort(jobs);
         Arrays.sort(worker);
 
-        int[] maxProfitAtDiff = new int[n];
-        maxProfitAtDiff[0] = job[0].profit;
-
-        for(int i=1; i<n; i++){
-            maxProfitAtDiff[i] = Math.max(maxProfitAtDiff[i-1], job[i].profit);
-        }
-
         int maxProfit = 0;
+        int i = 0;
+        int best = 0;
 
-        for(int w: worker){
-            int idx = binarySearch(job, w);
-            if(idx != -1){
-                maxProfit += maxProfitAtDiff[idx];
+        for (int ability : worker) {
+            while (i < n && ability >= jobs[i].diff) {
+                best = Math.max(best, jobs[i].profit);
+                i++;
             }
+            maxProfit += best;
         }
 
         return maxProfit;
-    }
-
-    private int binarySearch(Pair[] job, int ability){
-        int left = 0;
-        int right = job.length-1;
-        while(left <= right){
-            int mid = left + (right - left) / 2;
-
-            if(job[mid].diff <= ability){
-                left = mid+1;
-            }
-            else{
-                right = mid - 1;
-            }
-        }
-
-        return right;
 
     }
 }
