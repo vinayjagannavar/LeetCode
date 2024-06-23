9e52edde22e64af0943ef5d7be4df3c261b8e5b7
Vinay Jagannavar <vjagannavar@gmail.com>
2024-06-22 21:35:11 -0400

    Time: 1 ms (100%), Space: 47.4 MB (59.99%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
@@ -1,21 +1,50 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
+//Approach: two pointer and Sliding Window:
+            // Its slightly better than prefix sum for this scenario as we are given binary array
+        return atMost(nums,goal) - atMost(nums,goal-1);
+    }
 
-        int[] presum = new int[nums.length+1];
-        int ans = 0;
-        int cnt = 0;
+    private int atMost(int[] nums, int goal){
+        if(goal<0){
+            return 0;
+        }
 
-        presum[0] = 1;
+        int l=0;
+        int r=0;
+        int count=0;
+        int ans=0;
 
-        for(int num: nums){
-            cnt += num;
-            if(cnt - goal >=0 ){
-                ans += presum[cnt - goal];
+        while(r<nums.length){
+            count += nums[r];
+            while(count > goal){
+                count -= nums[l];
+                l++;
             }
-            presum[cnt]++;
+            ans += r - l + 1;
+            r++;
         }
 
         return ans;
+    }
+
+// Approach: Prefix Sum using Map and Array:
+
+        // int[] presum = new int[nums.length+1];
+        // int ans = 0;
+        // int cnt = 0;
+
+        // presum[0] = 1;
+
+        // for(int num: nums){
+        //     cnt += num;
+        //     if(cnt - goal >=0 ){
+        //         ans += presum[cnt - goal];
+        //     }
+        //     presum[cnt]++;
+        // }
+
+        // return ans;
         // int count = 0;
         // int sum = 0;
         // Map<Integer, Integer> seen = new HashMap<>();
@@ -27,5 +56,5 @@ class Solution {
         // }
 
         // return count;
-    }
+
 }
