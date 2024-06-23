04fe6e5b2fcd6b816cf77d6d4ce72970389a4b0f
Vinay Jagannavar <vjagannavar@gmail.com>
2024-06-22 20:54:24 -0400

    Time: 4 ms (99.1%), Space: 53 MB (94.54%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.java
@@ -1,17 +1,31 @@
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<>();
-        int preSum = 0;
-        int ans = 0;
-        map.put(0,1);
 
-        for(int i=0; i<nums.length; i++){
-            preSum += nums[i] % 2 == 1? 1 : 0;
-            if(map.containsKey(preSum - k)){
-                ans += map.get(preSum - k);
+        int preSum[] = new int[nums.length + 1];
+        int ans = 0;
+        int cnt = 0;
+        preSum[0]=1;
+
+        for(int num: nums){
+            cnt += num & 1;
+            if(cnt - k >=0){
+                ans += preSum[cnt-k];
             }
-            map.put(preSum,map.getOrDefault(preSum,0)+1);
+            preSum[cnt]++;
+
         }
+        // Map<Integer, Integer> map = new HashMap<>();
+        // int preSum = 0;
+        // int ans = 0;
+        // map.put(0,1);
+
+        // for(int i=0; i<nums.length; i++){
+        //     preSum += nums[i] % 2 == 1? 1 : 0;
+        //     if(map.containsKey(preSum - k)){
+        //         ans += map.get(preSum - k);
+        //     }
+        //     map.put(preSum,map.getOrDefault(preSum,0)+1);
+        // }
 
         return ans;
     }
