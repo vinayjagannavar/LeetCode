11f917b0cfdfb4581f4a0c20220670dd5ad6d43a
Vinay Jagannavar <vjagannavar@gmail.com>
2024-06-28 14:38:13 -0400

    Time: 7 ms (87.57%), Space: 62.4 MB (84.32%) - LeetHub

--- 2379-maximum-total-importance-of-roads/2379-maximum-total-importance-of-roads.java
+++ 2379-maximum-total-importance-of-roads/2379-maximum-total-importance-of-roads.java
@@ -2,13 +2,13 @@ class Solution {
     public long maximumImportance(int n, int[][] roads) {
         int[] freq = new int[n];
         for (int[] road: roads) {
-            ++freq[road[0]];
-            ++freq[road[1]];
+            freq[road[0]]++;
+            freq[road[1]]++;
         }
         Arrays.sort(freq);
 
         long importanceSum = 0;
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < n; i++) {
             importanceSum += freq[i] * (i + 1L);
         }
 
