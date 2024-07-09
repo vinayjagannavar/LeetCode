a0247feb529aa538d6977fd30f15c5fbf060b67d
Vinay Jagannavar <vjagannavar@gmail.com>
2024-07-08 20:51:45 -0400

    Time: 0 ms (100%), Space: 40.5 MB (50.53%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
@@ -1,23 +1,32 @@
 class Solution {
+
     public int findTheWinner(int n, int k) {
-        Queue<Integer> queue = new LinkedList<>();
-        int count = 0;
-
-        for(int i=1; i<=n; i++){
-            queue.offer(i);
-        }
-
-        while(queue.size() > 1){
-            int num = queue.poll();
-            count++;
-            if(count != k){
-                queue.offer(num);
-            }
-            if(count==k){
-                count=0;
-            }
-        }
-
-        return queue.poll();
+        return josephusHelper(n,k) + 1;
     }
+    public int josephusHelper(int n,int k){
+    if(n==1) return 0;
+      return (josephusHelper(n-1,k)+k) % n;
+    }
+
+    // public int findTheWinner(int n, int k) {
+    //     Queue<Integer> queue = new LinkedList<>();
+    //     int count = 0;
+
+    //     for(int i=1; i<=n; i++){
+    //         queue.offer(i);
+    //     }
+
+    //     while(queue.size() > 1){
+    //         int num = queue.poll();
+    //         count++;
+    //         if(count != k){
+    //             queue.offer(num);
+    //         }
+    //         if(count==k){
+    //             count=0;
+    //         }
+    //     }
+
+    //     return queue.poll();
+    // }
 }
