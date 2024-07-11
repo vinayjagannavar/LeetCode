70fe2b682f87fa8094f6a7096846ce37bde112b6
Vinay Jagannavar <vjagannavar@gmail.com>
2024-07-11 00:27:29 -0400

    Time: 0 ms (100%), Space: 41.4 MB (41.68%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,20 +1,36 @@
+// class Solution {
+//     public boolean isSubsequence(String s, String t) {
+
+//         int index_s = 0;
+//         int index_t = 0;
+
+//         if(s.length()==0){
+//             return true;
+//         }
+
+//         while(index_s<s.length() && index_t<t.length()){
+//             if(s.charAt(index_s) == t.charAt(index_t)){
+//                 index_s++;
+//             }
+//             index_t++;
+//         }
+
+//         return index_s == s.length();
+//     }   
+// }
+
 class Solution {
     public boolean isSubsequence(String s, String t) {
-
-        int index_s = 0;
-        int index_t = 0;
-
-        if(s.length()==0){
-            return true;
+        int p1 = 0,p2 = 0;
+        char a1[] = s.toCharArray();
+        char a2[] = t.toCharArray();
+        int n1 = a1.length, n2 = a2.length;
+        while(p1 < n1 && p2 < n2)
+        {
+            if(a1[p1] == a2[p2])
+                p1++;
+            p2++;
         }
-
-        while(index_s<s.length() && index_t<t.length()){
-            if(s.charAt(index_s) == t.charAt(index_t)){
-                index_s++;
-            }
-            index_t++;
-        }
-
-        return index_s == s.length();
+        return p1 == n1;
     }
 }
