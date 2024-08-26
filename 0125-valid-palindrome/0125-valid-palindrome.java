9b556ce2df6adc1db89312ea9c8832525e96d536
Vinay Jagannavar <vjagannavar@gmail.com>
2024-08-26 19:31:18 -0400

    Time: 1 ms (100%), Space: 43 MB (70.22%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,18 +1,28 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String cleanedStr = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-        
-        int i = 0;
-        int j = cleanedStr.length() - 1;
-
-        while (i < j) {
-            if (cleanedStr.charAt(i) != cleanedStr.charAt(j)) {
-                return false;
+        int i=0;
+        int j=s.length()-1;
+        while(j>i){
+            char f=s.charAt(i);
+            char l=s.charAt(j);
+            if(!((f>='A' && f<='Z') || (f>='a' && f<='z') || (f>='0' && f<='9'))){
+                i++;
+                continue;
             }
+            if(!((l>='A' && l<='Z') || (l>='a' && l<='z') || (l>='0' && l<='9'))){
+                j--;
+                continue;
+            }
+            if(f>='A' && f<='Z'){
+                f=(char) (f-'A'+'a');
+            }
+            if(l>='A' && l<='Z'){
+                l=(char) (l-'A'+'a');
+            }
+            if(f!=l) return false;
             i++;
             j--;
         }
-
         return true;
     }
 }
