f7962a4c4db178941b88ae7ad40ebf63ed9fd8cc
Vinay Jagannavar <vjagannavar@gmail.com>
2024-08-26 18:59:45 -0400

    Time: 1 ms (33.7%), Space: 44.7 MB (52.09%) - LeetHub

---
+++ 0776-n-ary-tree-postorder-traversal/0776-n-ary-tree-postorder-traversal.java
@@ -0,0 +1,44 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    List<Integer> output = new ArrayList<>();
+
+    public List<Integer> postorder(Node root) {
+        if(root==null){
+            return output;
+        }
+        dfs(root);
+        return output;
+    }
+
+    private void dfs(Node root){
+
+        if(root.children.size()==0){
+            output.add(root.val);
+            return;
+        }
+
+        for(Node child : root.children){
+            dfs(child);
+        }
+
+        output.add(root.val);
+    }
+}
