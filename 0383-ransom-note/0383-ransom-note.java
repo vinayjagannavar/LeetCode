96ebedd04f5c0dec2984ddfd7b6e96f670e98ac6
Vinay Jagannavar <vjagannavar@gmail.com>
2024-09-02 19:32:31 -0400

    Time: 1 ms (98.89%), Space: 44.7 MB (71.01%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,18 +1,37 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        Map<Character, Integer> map = new HashMap<>();
+        // Map<Character, Integer> map = new HashMap<>();
 
-        for(char c : magazine.toCharArray()){
-            map.put(c, map.getOrDefault(c, 0) + 1);
+        // for(char c : magazine.toCharArray()){
+        //     map.put(c, map.getOrDefault(c, 0) + 1);
+        // }
+
+        // for(char c : ransomNote.toCharArray()){
+        //     if(map.containsKey(c) && map.get(c)>0){
+        //         map.put(c,map.get(c)-1);
+        //     }
+        //     else{
+        //         return false;
+        //     }
+        // }
+
+        // return true;
+
+        int[] counter = new int[26];
+
+        if(ransomNote.length() > magazine.length()){
+            return false;
         }
 
-        for(char c : ransomNote.toCharArray()){
-            if(map.containsKey(c) && map.get(c)>0){
-                map.put(c,map.get(c)-1);
-            }
-            else{
+        for(char c: magazine.toCharArray()){
+            counter[c-'a'] += 1;
+        }
+
+        for(char ch: ransomNote.toCharArray()){
+            if(counter[ch - 'a'] == 0){
                 return false;
             }
+            counter[ch - 'a'] -= 1;
         }
 
         return true;
