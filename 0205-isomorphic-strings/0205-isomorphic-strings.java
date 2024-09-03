b4f0fc942a2251f0486f862b63248f563902a62e
Vinay Jagannavar <vjagannavar@gmail.com>
2024-09-02 20:51:53 -0400

    Time: 12 ms (45.64%), Space: 42.5 MB (27.31%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -3,16 +3,16 @@ class Solution {
 
         if(s.length()!=t.length())
         return false;
-        
+
         Map<Character,Character> map = new HashMap<>();
 
         for(int i=0; i<s.length(); i++){
             if(!map.containsKey(s.charAt(i))){
-                if(!map.containsValue(t.charAt(i))){
-                    map.put(s.charAt(i), t.charAt(i));
+                if(map.containsValue(t.charAt(i))){    
+                    return false;
                 }
                 else{
-                    return false;
+                    map.put(s.charAt(i), t.charAt(i));
                 }   
             }
             else{
