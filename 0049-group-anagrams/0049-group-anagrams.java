e254d95f1dbad809d7940d0351e55f090f259400
Vinay Jagannavar <vjagannavar@gmail.com>
2024-09-03 19:30:51 -0400

    Time: 14 ms (24.28%), Space: 48.1 MB (27.37%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,44 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        Map<String,List<String>> map = new HashMap();
+
+        if(strs.length == 0){
+            return new ArrayList<>();
+        }
+
+        for(String str: strs){
+            String freqString = getFrequencyString(str);
+            if(!map.containsKey(freqString)){
+                List<String> list = new ArrayList<>();
+                list.add(str);
+                map.put(freqString,list);
+            }
+            else{
+                map.get(freqString).add(str);
+            }
+        }
+
+        return new ArrayList<>(map.values());
+    }
+
+    private String getFrequencyString(String str){
+        int alpha[] = new int[26];
+
+        for(char c: str.toCharArray()){
+            alpha[c - 'a'] += 1;
+        }
+
+        char c = 'a';
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0; i<alpha.length; i++){
+            sb.append(c);
+            sb.append(alpha[i]);
+            c++;
+        }
+
+        return sb.toString();
+    }
+}
