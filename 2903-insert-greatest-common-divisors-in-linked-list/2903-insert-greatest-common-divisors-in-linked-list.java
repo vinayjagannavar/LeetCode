2dfc94c590af3383cf18f8e4b729f8b3f1aff06d
Vinay Jagannavar <vjagannavar@gmail.com>
2024-09-10 17:25:06 -0400

    Time: 2 ms (71.1%), Space: 45.3 MB (21.62%) - LeetHub

--- 2903-insert-greatest-common-divisors-in-linked-list/2903-insert-greatest-common-divisors-in-linked-list.java
+++ 2903-insert-greatest-common-divisors-in-linked-list/2903-insert-greatest-common-divisors-in-linked-list.java
@@ -1,13 +1,3 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode insertGreatestCommonDivisors(ListNode head) {
         if (head == null || head.next == null) {
@@ -17,7 +7,7 @@ class Solution {
         ListNode current = head;
 
         while (current != null && current.next != null) {
-            int gcdValue = findGCD(current.val, current.next.val);
+            int gcdValue = gcd(current.val, current.next.val);
             ListNode newNode = new ListNode(gcdValue);
 
             newNode.next = current.next;
@@ -29,12 +19,10 @@ class Solution {
         return head;
     }
 
-    private static int findGCD(int a, int b) {
-        // Base case: if b is 0, GCD is a
+    private int gcd(int a, int b) {
         if (b == 0) {
             return a;
         }
-        // Recursively call the method with b and the remainder of a / b
-        return findGCD(b, a % b);
+        return gcd(b, a % b);
     }
 }
