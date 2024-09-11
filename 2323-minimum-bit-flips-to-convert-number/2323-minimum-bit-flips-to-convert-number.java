845e1989d6a80041d07b27a33946f5c72dc24314
Vinay Jagannavar <vjagannavar@gmail.com>
2024-09-11 12:09:10 -0400

    Time: 0 ms (100%), Space: 40.1 MB (96.51%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.java
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.java
@@ -1,20 +1,36 @@
 class Solution {
+
     public int minBitFlips(int start, int goal) {
-        int length = Math.max(Integer.toBinaryString(start).length(), Integer.toBinaryString(goal).length());
-        int flip=0;
-        while(length>0){
-            int bitStart = start & 1;
-            int bitGoal = goal & 1;
+        int ans = 0; 
+        // XOR will give 1 where the bits are different
+        int xor = start ^ goal;
 
-            if(bitGoal!=bitStart){
-                flip+=1;
-            }
+        while(xor!=0){
+            // If the last bit is 1, increment count 
+            ans += xor & 1;
 
-            start >>=1;
-            goal >>=1;
-            length--;
+            // Right-shift to check the next bit
+            xor >>=1;
         }
-
-        return flip;
+        return ans;
     }
+
+    // public int minBitFlips(int start, int goal) {
+    //     int length = Math.max(Integer.toBinaryString(start).length(), Integer.toBinaryString(goal).length());
+    //     int flip=0;
+    //     while(length>0){
+    //         int bitStart = start & 1;
+    //         int bitGoal = goal & 1;
+
+    //         if(bitGoal!=bitStart){
+    //             flip+=1;
+    //         }
+
+    //         start >>=1;
+    //         goal >>=1;
+    //         length--;
+    //     }
+
+    //     return flip;
+    // }
 }
