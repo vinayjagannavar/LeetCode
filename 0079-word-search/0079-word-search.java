2e973beeb5204273514d371614b52e844a423a15
Vinay Jagannavar <vjagannavar@gmail.com>
2024-09-19 21:32:42 -0400

    Time: 259 ms (15.28%), Space: 41.9 MB (27.43%) - LeetHub

---
+++ 0079-word-search/0079-word-search.java
@@ -0,0 +1,45 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        int n = board.length;
+        int m = board[0].length;
+
+        boolean[][] visited = new boolean[n][m];
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(board[i][j] == word.charAt(0)){
+                    if(dfs(board, word, visited, i, j, 0)){
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+        
+    }
+
+    private boolean dfs(char[][] board, String word, boolean[][] visited, int row, int col, int index){
+
+        if(index == word.length()){
+            return true;
+        }
+        
+        if(row<0 || row >= board.length || col<0 || col >= board[0].length || visited[row][col] ||board[row][col] != word.charAt(index)){
+            return false;
+        }
+
+        visited[row][col] = true;
+
+        if(dfs(board, word, visited, row+1, col, index+1) ||
+        dfs(board, word, visited, row, col+1, index+1) ||
+        dfs(board, word, visited, row-1, col, index+1) ||
+        dfs(board, word, visited, row, col-1, index+1)){
+            return true;
+        }
+
+        visited[row][col] = false;
+
+        return false;
+    }
+}
