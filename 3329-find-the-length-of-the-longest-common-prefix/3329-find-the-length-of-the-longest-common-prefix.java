2c4072fadfef8fdfb7107fa1a293e6a87605d911
Vinay Jagannavar <vjagannavar@gmail.com>
2024-09-24 17:15:59 -0400

    Time: 33 ms (93.25%), Space: 55.3 MB (84.43%) - LeetHub

--- 3329-find-the-length-of-the-longest-common-prefix/3329-find-the-length-of-the-longest-common-prefix.java
+++ 3329-find-the-length-of-the-longest-common-prefix/3329-find-the-length-of-the-longest-common-prefix.java
@@ -1,14 +1,76 @@
 
+// class Solution {
+//     private final TrieNode root;
+
+//     class TrieNode{
+//         Map<Character,TrieNode> children;
+//         boolean endOfWord;
+
+//         public TrieNode(){
+//             children = new HashMap();
+//             endOfWord = false;
+//         }
+//     }
+
+//     public Solution() {
+//         root = new TrieNode();
+//     }
+
+//     public void insert(String word){
+//         TrieNode current = root;
+//         for(char c: word.toCharArray()){
+//             TrieNode node = current.children.get(c);
+
+//             if(node == null){
+//                 node = new TrieNode();
+//                 current.children.put(c,node);
+//             }
+
+//             current = node;
+//         }
+
+//         current.endOfWord = true;
+//     }
+
+//     public int search(String word){
+//         int count = 0;
+//         TrieNode current = root;
+//         for(char c: word.toCharArray()){
+//             if(current.children.get(c) !=null){
+//                 count++;
+//                 current = current.children.get(c);
+//             }
+//             else{
+//                 return count;
+//             }
+//         }
+//         return count;  
+//     }
+
+
+//     public int longestCommonPrefix(int[] arr1, int[] arr2) {
+//         int longestLength = 0;
+//         for(int num: arr1){
+//             insert(String.valueOf(num));
+//         }
+
+//         for(int num: arr2){
+//             longestLength = Math.max(longestLength, search(String.valueOf(num)));
+//         }
+
+//         return longestLength;
+//     }
+// }
+
+
 class Solution {
-    private final TrieNode root;
+    TrieNode root;
 
     class TrieNode{
-        Map<Character,TrieNode> children;
-        boolean endOfWord;
+       TrieNode[] children;
 
         public TrieNode(){
-            children = new HashMap();
-            endOfWord = false;
+            children = new TrieNode[10];
         }
     }
 
@@ -19,26 +81,24 @@ class Solution {
     public void insert(String word){
         TrieNode current = root;
         for(char c: word.toCharArray()){
-            TrieNode node = current.children.get(c);
+            TrieNode node = current.children[c-'0'];
 
             if(node == null){
                 node = new TrieNode();
-                current.children.put(c,node);
+                current.children[c-'0']=node;
             }
 
             current = node;
         }
-
-        current.endOfWord = true;
     }
 
     public int search(String word){
         int count = 0;
         TrieNode current = root;
         for(char c: word.toCharArray()){
-            if(current.children.get(c) !=null){
+            if(current.children[c-'0'] !=null){
                 count++;
-                current = current.children.get(c);
+                current = current.children[c-'0'];
             }
             else{
                 return count;
