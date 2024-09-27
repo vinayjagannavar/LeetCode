9f85f3ea52df3731defb793d52dc815a393b970b
Vinay Jagannavar <vjagannavar@gmail.com>
2024-09-26 20:04:03 -0400

    Time: 11 ms (100%), Space: 45.6 MB (42.53%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.java
+++ 0729-my-calendar-i/0729-my-calendar-i.java
@@ -1,23 +1,75 @@
+// class MyCalendar {
+//     List<int[]> calendar;
+
+//     public MyCalendar() {
+//         calendar = new ArrayList<>();
+//     }
+    
+//     public boolean book(int start, int end) {
+        
+//         for (int[] event : calendar) {
+            
+//             if (event[0] < end && start < event[1]) {
+//                 return false; 
+//             }
+//         }
+        
+//         calendar.add(new int[]{start, end});
+//         return true;
+//     }
+// }
+
 class MyCalendar {
-    List<int[]> calendar;
+
+    class Node {
+        int start;
+        int end;
+        Node left,right;
+
+        public Node(int start,int end){
+            this.start = start;
+            this.end = end;
+        }
+    }
+
+    Node root;
 
     public MyCalendar() {
-        calendar = new ArrayList<>();
+        
     }
     
     public boolean book(int start, int end) {
-        
-        for (int[] event : calendar) {
-            
-            if (event[0] < end && start < event[1]) {
-                return false; 
-            }
+        if(root == null){
+            root = new Node(start,end);
+            return true;
         }
-        
-        calendar.add(new int[]{start, end});
-        return true;
+        Node curr = root;
+        while(curr != null){
+            if(end <= curr.start){
+                if(curr.left == null){
+                    curr.left = new Node(start,end);
+                    return true;
+                }
+                curr = curr.left;
+            }
+            else if(start >= curr.end){
+                if(curr.right == null){
+                    curr.right = new Node(start,end);
+                    return true;
+                }
+                curr = curr.right;
+            }
+            else return false;
+        }
+        return false;
     }
 }
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar obj = new MyCalendar();
+ * boolean param_1 = obj.book(start,end);
+ */
 
 /**
  * Your MyCalendar object will be instantiated and called as such:
