6c6e434c85bcd412d38e01c7750573fa2a63d555
Vinay Jagannavar <vjagannavar@gmail.com>
2024-09-28 16:14:51 -0400

    Time: 5 ms (37.42%), Space: 44.8 MB (46.36%) - LeetHub

--- 0859-design-circular-deque/0859-design-circular-deque.java
+++ 0859-design-circular-deque/0859-design-circular-deque.java
@@ -98,65 +98,65 @@
 //     }
 // }
 
-
 class MyCircularDeque {
-    private LinkedList<Integer> dq;
-    private int n;
-
+     int[] arr;
+     int f,r,c;
     public MyCircularDeque(int k) {
-        dq = new LinkedList<>();
-        n = k;
+        arr=new int[k];
+        f=-1;
+        r=-1;
+        c=k;
     }
-
+    
     public boolean insertFront(int value) {
-        if (dq.size() < n) {
-            dq.addFirst(value);
-            return true;
-        }
-        return false;
+        if(isFull())return false;
+        if(isEmpty())f=r=0;
+        else f=(f-1+c)%c;
+        arr[f]=value;
+        return true;
     }
-
+    
     public boolean insertLast(int value) {
-        if (dq.size() < n) {
-            dq.addLast(value);
-            return true;
-        }
-        return false;
+        if(isFull())return false;
+        if(isEmpty())f=r=0;
+        else r=(r+1)%c;
+        arr[r]=value;
+        return true;
     }
-
+    
     public boolean deleteFront() {
-        if (!dq.isEmpty()) {
-            dq.removeFirst();
-            return true;
-        }
-        return false;
+        if(isEmpty())return false;
+        if(f==r)f=r=-1;
+        else f=(f+1)%(c);
+        return true;
     }
-
+    
     public boolean deleteLast() {
-        if (!dq.isEmpty()) {
-            dq.removeLast();
-            return true;
-        }
-        return false;
+        if(isEmpty())return false;
+        if(f==r)f=r=-1;
+        else r=(r-1+c)%c;
+        return true;
     }
-
+    
     public int getFront() {
-        return dq.isEmpty() ? -1 : dq.getFirst();
+        if(isEmpty())return -1;
+        return arr[f];
     }
-
+    
     public int getRear() {
-        return dq.isEmpty() ? -1 : dq.getLast();
+        if(isEmpty())return -1;
+        return arr[r];
     }
-
+    
     public boolean isEmpty() {
-        return dq.isEmpty();
+        return f==-1;
     }
-
+    
     public boolean isFull() {
-        return dq.size() == n;
+        return ((f-1+c)%c)==r;
+        //return ((f-1+c)%c)==r;
     }
 }
-
 
 /**
  * Your MyCircularDeque object will be instantiated and called as such:
