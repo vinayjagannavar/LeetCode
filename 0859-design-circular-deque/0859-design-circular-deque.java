cd02770f264ff805f8a06737f6471014c2a75e52
Vinay Jagannavar <vjagannavar@gmail.com>
2024-09-28 16:13:44 -0400

    Time: 5 ms (37.42%), Space: 45.1 MB (18.03%) - LeetHub

--- 0859-design-circular-deque/0859-design-circular-deque.java
+++ 0859-design-circular-deque/0859-design-circular-deque.java
@@ -1,102 +1,162 @@
-class MyCircularDeque {
-    int size;
-    int count = 0;
+// class MyCircularDeque {
+//     int size;
+//     int count = 0;
 
-    class ListNode{
-        int val;
-        ListNode next;
-        ListNode prev;
+//     class ListNode{
+//         int val;
+//         ListNode next;
+//         ListNode prev;
 
-        public ListNode(int val){
-            this.val = val;
-        }
-    }
+//         public ListNode(int val){
+//             this.val = val;
+//         }
+//     }
 
-    ListNode head;
-    ListNode tail;
-    public MyCircularDeque(int k) {
-        size = k;
-        head = new ListNode(-1);
-        tail = new ListNode(-1);
-        head.next = tail;
-        tail.prev = head;
-    }
+//     ListNode head;
+//     ListNode tail;
+//     public MyCircularDeque(int k) {
+//         size = k;
+//         head = new ListNode(-1);
+//         tail = new ListNode(-1);
+//         head.next = tail;
+//         tail.prev = head;
+//     }
     
-    public boolean insertFront(int value) {
-        if(count<size){
-            ListNode current = head.next;
-            ListNode newNode = new ListNode(value);
-            head.next = newNode;
-            newNode.next = current;
-            newNode.prev = head;
-            current.prev = newNode;
-            count++;
-            return true;
-        }
-        else{
-            return false;
-        }
+//     public boolean insertFront(int value) {
+//         if(count<size){
+//             ListNode current = head.next;
+//             ListNode newNode = new ListNode(value);
+//             head.next = newNode;
+//             newNode.next = current;
+//             newNode.prev = head;
+//             current.prev = newNode;
+//             count++;
+//             return true;
+//         }
+//         else{
+//             return false;
+//         }
         
-    }
+//     }
     
+//     public boolean insertLast(int value) {
+//         if(count<size){
+//             ListNode current = tail.prev;
+//             ListNode newNode = new ListNode(value);
+//             tail.prev = newNode;
+//             newNode.prev = current;
+//             newNode.next = tail;
+//             current.next = newNode;
+//             count++;
+//             return true;
+//         }
+//         else{
+//             return false;
+//         }
+//     }
+    
+//     public boolean deleteFront() {
+//         if(count>0){
+//             ListNode current = head.next.next;
+//             head.next = current;
+//             current.prev = head;
+//             count--;
+//             return true;
+//         }
+//         else{
+//             return false;
+//         }
+//     }
+    
+//     public boolean deleteLast() {
+//         if(count>0){
+//             ListNode current = tail.prev.prev;
+//             tail.prev = current;
+//             current.next = tail;
+//             count--;
+//             return true;
+//         }
+//         else{
+//             return false;
+//         }
+//     }
+    
+//     public int getFront() {
+//         return head.next.val;
+//     }
+    
+//     public int getRear() {
+//         return tail.prev.val;
+//     }
+    
+//     public boolean isEmpty() {
+//         return count<=0? true : false;
+//     }
+    
+//     public boolean isFull() {
+//         return count==size? true : false;
+//     }
+// }
+
+
+class MyCircularDeque {
+    private LinkedList<Integer> dq;
+    private int n;
+
+    public MyCircularDeque(int k) {
+        dq = new LinkedList<>();
+        n = k;
+    }
+
+    public boolean insertFront(int value) {
+        if (dq.size() < n) {
+            dq.addFirst(value);
+            return true;
+        }
+        return false;
+    }
+
     public boolean insertLast(int value) {
-        if(count<size){
-            ListNode current = tail.prev;
-            ListNode newNode = new ListNode(value);
-            tail.prev = newNode;
-            newNode.prev = current;
-            newNode.next = tail;
-            current.next = newNode;
-            count++;
+        if (dq.size() < n) {
+            dq.addLast(value);
             return true;
         }
-        else{
-            return false;
-        }
+        return false;
     }
-    
+
     public boolean deleteFront() {
-        if(count>0){
-            ListNode current = head.next.next;
-            head.next = current;
-            current.prev = head;
-            count--;
+        if (!dq.isEmpty()) {
+            dq.removeFirst();
             return true;
         }
-        else{
-            return false;
-        }
+        return false;
     }
-    
+
     public boolean deleteLast() {
-        if(count>0){
-            ListNode current = tail.prev.prev;
-            tail.prev = current;
-            current.next = tail;
-            count--;
+        if (!dq.isEmpty()) {
+            dq.removeLast();
             return true;
         }
-        else{
-            return false;
-        }
+        return false;
     }
-    
+
     public int getFront() {
-        return head.next.val;
+        return dq.isEmpty() ? -1 : dq.getFirst();
     }
-    
+
     public int getRear() {
-        return tail.prev.val;
+        return dq.isEmpty() ? -1 : dq.getLast();
     }
-    
+
     public boolean isEmpty() {
-        return count<=0? true : false;
+        return dq.isEmpty();
     }
-    
+
     public boolean isFull() {
-        return count==size? true : false;
+        return dq.size() == n;
     }
 }
+
 
 /**
  * Your MyCircularDeque object will be instantiated and called as such:
