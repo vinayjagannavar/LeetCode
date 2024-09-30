532ea0d4d6383e623d8d1a10e0b335e4f8d0482a
Vinay Jagannavar <vjagannavar@gmail.com>
2024-09-30 14:58:17 -0400

    Time: 4 ms (100%), Space: 45.7 MB (42.51%) - LeetHub

--- 1497-design-a-stack-with-increment-operation/1497-design-a-stack-with-increment-operation.java
+++ 1497-design-a-stack-with-increment-operation/1497-design-a-stack-with-increment-operation.java
@@ -1,71 +1,100 @@
+// class CustomStack {
+
+//     class ListNode{
+//         int val;
+//         ListNode next;
+
+//         public ListNode(int val){
+//             this.val = val;
+//         }
+//     }
+
+//     private ListNode head;
+//     int maxSize;
+//     int size = 0;
+//     public CustomStack(int maxSize) {
+//         this.maxSize = maxSize;
+//         head = new ListNode(-1);
+//     }
+    
+//     public void push(int x) {
+//         if(size < maxSize){
+//             ListNode node = new ListNode(x);
+//             if(head.next == null){
+//                 head.next = node;
+//             }
+//             else{
+//                 node.next = head.next;
+//                 head.next = node;
+//             }
+//             size++;
+//         }
+
+//         return;
+//     }
+    
+//     public int pop() {
+//         if(size>0){
+//             ListNode node = head.next;
+//             head.next = head.next.next;
+//             node.next = null;
+//             size--;
+//             return node.val;
+//         }
+        
+//         return -1;
+//     }
+    
+//     public void increment(int k, int val) {
+//         if(k>=size){
+//             ListNode current = head.next;
+//             while(current != null){
+//                 current.val = val+current.val;
+//                 current = current.next;
+//             }
+//         }
+//         else{
+//             int skip = size - k;
+//             ListNode current = head.next;
+//             while(skip != 0){
+//                 current = current.next;
+//                 skip--;
+//             }
+
+//             while(current != null){
+//                 current.val = val+current.val;
+//                 current = current.next;
+//             }
+
+//         }
+//     }
+// }
+
+
 class CustomStack {
+    int[] stack;
+    int top;
 
-    class ListNode{
-        int val;
-        ListNode next;
-
-        public ListNode(int val){
-            this.val = val;
-        }
-    }
-
-    private ListNode head;
-    int maxSize;
-    int size = 0;
     public CustomStack(int maxSize) {
-        this.maxSize = maxSize;
-        head = new ListNode(-1);
+        stack = new int[maxSize];
+        top = -1;
     }
     
     public void push(int x) {
-        if(size < maxSize){
-            ListNode node = new ListNode(x);
-            if(head.next == null){
-                head.next = node;
-            }
-            else{
-                node.next = head.next;
-                head.next = node;
-            }
-            size++;
-        }
-
-        return;
+        if(top < stack.length-1){
+            stack[++top] = x;
+        }        
     }
     
     public int pop() {
-        if(size>0){
-            ListNode node = head.next;
-            head.next = head.next.next;
-            node.next = null;
-            size--;
-            return node.val;
-        }
         
-        return -1;
+        return top>-1?stack[top--]:-1;
     }
     
     public void increment(int k, int val) {
-        if(k>=size){
-            ListNode current = head.next;
-            while(current != null){
-                current.val = val+current.val;
-                current = current.next;
-            }
-        }
-        else{
-            int skip = size - k;
-            ListNode current = head.next;
-            while(skip != 0){
-                current = current.next;
-                skip--;
-            }
-
-            while(current != null){
-                current.val = val+current.val;
-                current = current.next;
-            }
-
+        k = Math.min(k, top+1);
+        for(int i = 0; i<k;i++){
+            stack[i]+=val;
         }
     }
 }
