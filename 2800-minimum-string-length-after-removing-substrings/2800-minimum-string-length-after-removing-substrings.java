a147008a0fda595ebf75c00fc9d9c6cdb0b70a9a
Vinay Jagannavar <vjagannavar@gmail.com>
2024-10-07 18:47:55 -0400

    Time: 1 ms (100%), Space: 43.4 MB (42.89%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.java
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.java
@@ -1,18 +1,33 @@
+// class Solution {
+//     public int minLength(String s) {
+//         Stack<Character> stack = new Stack<>();
+
+//         for(char c : s.toCharArray()){
+//             if(!stack.isEmpty()){
+//                 if(c=='B' && stack.peek()=='A' || c=='D' && stack.peek()=='C'){
+//                 stack.pop();
+//                 continue;
+//                 }
+//             }
+            
+//             stack.push(c);
+//         }
+
+//         return stack.size();
+//     }
+// }
+
 class Solution {
     public int minLength(String s) {
-        Stack<Character> stack = new Stack<>();
-
-        for(char c : s.toCharArray()){
-            if(!stack.isEmpty()){
-                if(c=='B' && stack.peek()=='A' || c=='D' && stack.peek()=='C'){
-                stack.pop();
-                continue;
-                }
+        int n = s.length(), p = -1;
+        char[] stack = new char[n];
+        for (char c : s.toCharArray()) {
+            if (p >= 0 && ((c == 'B' && stack[p] == 'A') || (c == 'D' && stack[p] == 'C'))) {
+                p--;
+            } else {
+                stack[++p] = c;
             }
-            
-            stack.push(c);
         }
-
-        return stack.size();
+        return p + 1;
     }
 }
