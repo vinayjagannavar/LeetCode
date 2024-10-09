192b3ec0eeb65581ee10e097c1fcd76d60039693
Vinay Jagannavar <vjagannavar@gmail.com>
2024-10-09 14:09:12 -0400

    Time: 0 ms (100%), Space: 41.6 MB (16.86%) - LeetHub

---
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        int open=0;
+        int close=0;
+
+        for(char c : s.toCharArray()){
+            if(c == '('){
+                open++;
+            }
+            else if(c == ')' && open>0){
+                open--;
+            }
+            else{
+                close++;
+            }
+        }
+
+        return open + close;
+    }
+}
