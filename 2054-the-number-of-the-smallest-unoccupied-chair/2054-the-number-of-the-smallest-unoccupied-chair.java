e74eda97d3c99ab4cb12e28b222f6d5101939667
Vinay Jagannavar <vjagannavar@gmail.com>
2024-10-11 19:31:38 -0400

    Time: 47 ms (67.8%), Space: 48.7 MB (66.67%) - LeetHub

--- 2054-the-number-of-the-smallest-unoccupied-chair/2054-the-number-of-the-smallest-unoccupied-chair.java
+++ 2054-the-number-of-the-smallest-unoccupied-chair/2054-the-number-of-the-smallest-unoccupied-chair.java
@@ -1,33 +1,42 @@
+import java.util.*;
+
 class Solution {
     public int smallestChair(int[][] times, int targetFriend) {
-        int[] targetInterval = times[targetFriend];
+        int targetArrival = times[targetFriend][0];
+        int targetDeparture = times[targetFriend][1];
 
         Arrays.sort(times, (a, b) -> a[0] - b[0]);
 
-        List<Integer> chair = new ArrayList<>();
+        PriorityQueue<Integer> availableChairs = new PriorityQueue<>();
 
+        PriorityQueue<int[]> occupiedChairs = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+
+        for (int i = 0; i < times.length; i++) {
+            availableChairs.offer(i);
+        }
+
+        // Process each friend in order of arrival
         for (int i = 0; i < times.length; i++) {
             int[] person = times[i];
             int arrival = person[0];
             int departure = person[1];
 
-            int j = 0;
-            for (j = 0; j < chair.size(); j++) {
-
-                if (chair.get(j) <= arrival) {
-                    chair.set(j, departure); 
-                    break;
-                }
+            // Free up any chairs from friends who have already left
+            while (!occupiedChairs.isEmpty() && occupiedChairs.peek()[0] <= arrival) {
+                int freedChair = occupiedChairs.poll()[1];
+                availableChairs.offer(freedChair); // Chair becomes available again
             }
 
-            if (j == chair.size()) {
-                chair.add(departure); 
-            }
+            // Assign the smallest available chair to the current person
+            int assignedChair = availableChairs.poll();
+            occupiedChairs.offer(new int[]{departure, assignedChair}); // Track when this chair will be free
 
-            if (person[0] == targetInterval[0] && person[1] == targetInterval[1]) {
-                return j;
+            // If it's the target friend, return the assigned chair number
+            if (arrival == targetArrival && departure == targetDeparture) {
+                return assignedChair;
             }
         }
-        return 0;
+
+        return -1; // This line shouldn't be reached
     }
 }
