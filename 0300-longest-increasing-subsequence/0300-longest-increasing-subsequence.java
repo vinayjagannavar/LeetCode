d42f7dba5ee6b472add381281b434ff324176ffc
Vinay Jagannavar <vjagannavar@gmail.com>
2024-10-18 21:00:44 -0400

    Time: 6 ms (89.19%), Space: 43.4 MB (99.63%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,21 +1,58 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int dp[] = new int[nums.length];
-        Arrays.fill(dp,1);
+        List<Integer> lis = new ArrayList<>();
 
-        for(int i=1; i<nums.length; i++){
-            for(int j=0; j<i ; j++){
-                if(nums[i] > nums[j]){
-                    dp[i] = Math.max(dp[i], dp[j]+1);
-                }
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            if (lis.isEmpty() || lis.get(lis.size() - 1) < nums[i]) {
+                lis.add(nums[i]);
+            } else {
+                int position = binarySearch(lis, nums[i]);
+                lis.set(position, nums[i]);
             }
         }
 
-        int max = 0;
-        for(int i : dp){
-            max = Math.max(max,i);
+        return lis.size();
+    }
+
+    private int binarySearch(List<Integer> lis, int num) {
+        int left = 0;
+        int right = lis.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (lis.get(mid) == num) {
+                return mid;
+            } else if (lis.get(mid) > num) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
         }
 
-        return max;
+        return left;
     }
 }
+
+// class Solution {
+//     public int lengthOfLIS(int[] nums) {
+//         int dp[] = new int[nums.length];
+//         Arrays.fill(dp,1);
+
+//         for(int i=1; i<nums.length; i++){
+//             for(int j=0; j<i ; j++){
+//                 if(nums[i] > nums[j]){
+//                     dp[i] = Math.max(dp[i], dp[j]+1);
+//                 }
+//             }
+//         }
+
+//         int max = 0;
+//         for(int i : dp){
+//             max = Math.max(max,i);
+//         }
+
+//         return max;
+//     }
+// }
