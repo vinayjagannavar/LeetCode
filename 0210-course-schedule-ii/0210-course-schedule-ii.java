427b3eef5f562b1a16f6fc836085542430ee0c6d
Vinay Jagannavar <vjagannavar@gmail.com>
2024-11-04 18:48:04 -0500

    Time: 6 ms (57.2%), Space: 46.9 MB (7.9%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,55 +1,48 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         List<List<Integer>> adj = new ArrayList<>();
-
         for(int i=0; i<numCourses; i++){
             adj.add(new ArrayList<>());
         }
 
-        for(int[] prereq : prerequisites){
-            adj.get(prereq[1]).add(prereq[0]);
+        for(int[] pre : prerequisites){
+            adj.get(pre[1]).add(pre[0]);
         }
 
-        boolean[] vis = new boolean[numCourses];
-        boolean[] recStack = new boolean[numCourses];
-        Stack<Integer> stack = new Stack<>();
+        int[] indegree = new int[numCourses];
+        Queue<Integer> queue = new LinkedList<>();
 
         for(int i=0; i<numCourses; i++){
-            if (!vis[i]) {
-                if (dfs(i, adj, vis, recStack, stack)) {
-                    return new int[0];
-                }
+            for(int neigh: adj.get(i)){
+                indegree[neigh]++;
             }
         }
 
-        int[] order = new int[numCourses];
+        for(int i=0; i<numCourses; i++){
+            if(indegree[i] == 0){
+                queue.offer(i);
+            }
+        }
+
+        int[] topo = new int[numCourses];
         int index = 0;
-        while (!stack.isEmpty()) {
-            order[index++] = stack.pop();
-        }
-        
-        return order;
-        
-    }
 
-    private boolean dfs(int start, List<List<Integer>> adj, boolean[] vis, boolean[] recStack, Stack<Integer> stack){
-        vis[start] = true;
-        recStack[start] = true;
+        while(!queue.isEmpty()){
+            int node = queue.poll();
+            topo[index++] = node;
 
-        for(int it: adj.get(start)){
-            if(!vis[it]){
-                if(dfs(it,adj,vis,recStack,stack)){
-                    return true;
-                }
-            }
-             else if (recStack[it]) {
-                return true; 
+            for(int neigh: adj.get(node)){
+                indegree[neigh]--;
+                if(indegree[neigh] == 0){
+                    queue.add(neigh);
+                }                
             }
         }
 
-        recStack[start] = false;
-        stack.push(start);
-
-        return false;
+        if (index != numCourses) {
+            return new int[0];
+        }
+        
+        return topo;
     }
 }
