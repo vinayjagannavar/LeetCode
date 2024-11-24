bbebce391a0f2d525041b32f529b8b0e40857136
Vinay Jagannavar <vjagannavar@gmail.com>
2024-11-24 18:25:31 -0500

    Time: 6 ms (91.11%), Space: 54.9 MB (75.46%) - LeetHub

---
+++ 2089-maximum-matrix-sum/2089-maximum-matrix-sum.java
@@ -0,0 +1,23 @@
+class Solution {
+    public long maxMatrixSum(int[][] matrix) {
+        int neg = 0;
+        long sum = 0;
+        long minVal = Long.MAX_VALUE;
+
+        for (int[] row : matrix) {
+            for (int val : row) {
+                if (val < 0) {
+                    neg++;
+                }
+                sum += Math.abs(val);
+                minVal = Math.min(minVal, Math.abs(val));
+            }
+        }
+
+        if (neg % 2 == 1) {
+            sum -= 2 * minVal;
+        }
+
+        return sum;
+    }
+}
