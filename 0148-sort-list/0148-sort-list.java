48461a2a99a14df1570a381331b8bae0c10b70d5
Vinay Jagannavar <vjagannavar@gmail.com>
2024-12-24 23:55:33 -0500

    Time: 9 ms (94.72%), Space: 56.6 MB (62.86%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.java
@@ -0,0 +1,65 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if(head == null || head.next == null){
+            return head;
+        }
+
+        ListNode mid = getMid(head);
+        ListNode left = head;
+        ListNode right = mid.next;
+        mid.next = null;
+
+        left = sortList(left);
+        right = sortList(right);
+
+        return mergeSort(left, right);
+    }
+
+    private ListNode getMid(ListNode head){
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    private ListNode mergeSort(ListNode left, ListNode right){
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+
+        while(left != null && right != null){
+            if(left.val <= right.val){
+                tail.next = left;
+                left = left.next;
+            }
+            else{
+                tail.next = right;
+                right = right.next;
+            }
+            tail = tail.next;
+        }
+
+        if(left != null){
+            tail.next = left;
+        }
+        else{
+            tail.next = right;
+        }
+
+        return dummy.next;
+    }
+}
