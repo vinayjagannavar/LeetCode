65e33d658c009d3543d2ff164466c7e820e3b0fd
Vinay Jagannavar <vjagannavar@gmail.com>
2025-05-09 18:53:30 -0400

    Time: 6 ms (98.02%), Space: 48.1 MB (44.79%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,64 +1,18 @@
-// class Solution {
-//     public List<List<String>> groupAnagrams(String[] strs) {
-
-//         Map<String,List<String>> map = new HashMap();
-
-//         if(strs.length == 0){
-//             return new ArrayList<>();
-//         }
-
-//         for(String str: strs){
-//             String freqString = getFrequencyString(str);
-//             if(!map.containsKey(freqString)){
-//                 List<String> list = new ArrayList<>();
-//                 list.add(str);
-//                 map.put(freqString,list);
-//             }
-//             else{
-//                 map.get(freqString).add(str);
-//             }
-//         }
-
-//         return new ArrayList<>(map.values());
-//     }
-
-//     private String getFrequencyString(String str){
-//         int alpha[] = new int[26];
-
-//         for(char c: str.toCharArray()){
-//             alpha[c - 'a'] += 1;
-//         }
-
-//         char c = 'a';
-
-//         StringBuilder sb = new StringBuilder();
-
-//         for(int i=0; i<alpha.length; i++){
-//             sb.append(c);
-//             sb.append(alpha[i]);
-//             c++;
-//         }
-
-//         return sb.toString();
-//     }
-// }
-
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> map = new HashMap<>();
-        
-        for (String word : strs) {
-            char[] chars = word.toCharArray();
+        Map<String,List<String>> map = new HashMap<>();
+
+        for(String str: strs){
+            char[] chars = str.toCharArray();
             Arrays.sort(chars);
-            String sortedWord = new String(chars);
-            
-            if (!map.containsKey(sortedWord)) {
-                map.put(sortedWord, new ArrayList<>());
+            String sortedChar = new String(chars);
+            if(!map.containsKey(sortedChar)){
+                map.put(sortedChar,new ArrayList<>());
             }
-            
-            map.get(sortedWord).add(word);
+            map.get(sortedChar).add(str);
         }
-        
+
         return new ArrayList<>(map.values());
+
     }
 }
