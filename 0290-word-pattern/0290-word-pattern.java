03307cc7fc10d4d378f6821f6fa79b341c2ab691
Vinay Jagannavar <vjagannavar@gmail.com>
2025-05-09 17:19:01 -0400

    Time: 1 ms (81.21%), Space: 41.6 MB (31.82%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -1,32 +1,31 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
+        Map<Character,String> map = new HashMap<>();
 
-        String[] words = s.split("\\s+");
+        String[] words = s.split(" ");
 
-        Map<Character, String> map = new HashMap<>();
-
-        if(pattern.length() != words.length) {
+        if(pattern.length() != words.length){
             return false;
         }
-        
-        // Iterate through each character of the strings
-        for(int i = 0; i < pattern.length(); i++) {
-            if(!map.containsKey(pattern.charAt(i))){
-                if(!map.containsValue(words[i])){
-                    map.put(pattern.charAt(i),words[i]);
+
+        for(int i=0; i<pattern.length(); i++){
+            char c = pattern.charAt(i);
+            String word = words[i];
+            if(!map.containsKey(c)){
+                if(!map.containsValue(word)){
+                    map.put(c, word);
                 }
                 else{
                     return false;
                 }
             }
             else{
-                if(!map.get(pattern.charAt(i)).equals(words[i])){
+                if(!map.get(c).equals(word)){
                     return false;
                 }
             }
         }
 
         return true;
-        
     }
 }
